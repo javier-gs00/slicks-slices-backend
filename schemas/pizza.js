216378d2fd99e348455cd429e87d4bbad9cc73dc
Javier Gutierrez <javier.guse@icloud.com>
2020-10-17 20:03:37 -0300

    Create relation between pizzas and toppings

--- schemas/pizza.js
+++ schemas/pizza.js
@@ -37,5 +37,29 @@ export default {
       validation: (Rule) => Rule.min(100).max(50000),
       // TODO: Add custom component
     },
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'topping' }] }],
+      description: 'Name of the pizza',
+    },
   ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    prepare: ({ title, media, ...toppings }) => {
+      // filter undefined toppings
+      const tops = Object.values(toppings).filter(Boolean);
+      // return the preview object for the pizza
+
+      return { title, media, subtitle: tops.join(', ') };
+    },
+  },
 };
